8b20a316827a84fe66853723266ea8fb04a7e704
Icemap <qizhi.wang@pingcap.com>
2023-03-17 22:56:20 +0800

    [Update] version date

--- src/main/java/com/pingcap/ossinsightcoss/controller/VersionController.java
+++ src/main/java/com/pingcap/ossinsightcoss/controller/VersionController.java
@@ -1,14 +1,24 @@
 package com.pingcap.ossinsightcoss.controller;
 
+import jakarta.annotation.PostConstruct;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.Date;
+
 @RestController
 @RequestMapping("/version")
 public class VersionController {
+
+    Date startTime = new Date();
+    @PostConstruct
+    public void init() {
+        startTime = new Date();
+    }
+
     @GetMapping
-    public String getVersion() {
-        return "0.0.5";
+    public Date getVersion() {
+        return startTime;
     }
 }
