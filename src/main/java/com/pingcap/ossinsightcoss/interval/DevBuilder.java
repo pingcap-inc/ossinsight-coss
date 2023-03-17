f70597e4597394d14f8dc7a0ec41c6875347bf00
Icemap <qizhi.wang@pingcap.com>
2023-03-17 22:54:19 +0800

    [Update] after migrate temp update

--- src/main/java/com/pingcap/ossinsightcoss/interval/DevBuilder.java
+++ src/main/java/com/pingcap/ossinsightcoss/interval/DevBuilder.java
@@ -112,8 +112,12 @@ public class DevBuilder {
     }
 
     // between each task, it will have 5m break.
-    @Scheduled(fixedDelay=5, timeUnit=TimeUnit.MINUTES)
+    @Scheduled(fixedDelay=1, timeUnit=TimeUnit.MINUTES)
     public void pickOneDevMonthlyDataTask() {
+        if (refreshDevMonthlyStack.isEmpty()) {
+            refreshDevMonthlyStack.addAll(cossInvestRepository.findAll());
+        }
+
         if (!refreshDevMonthlyStack.isEmpty()) {
             cossDevMonthlyRepository.transferCOSSDevMonthlyBeanByRepoName(
                     refreshDevDailyStack.pop().getGithubName()
